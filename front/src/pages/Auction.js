3bd6075b998b37b64300dc20cc85b1f583ff3fc8
sangbeomji <sangbeomji@gmail.com>
2022-01-09 16:08:45 +0900

    auction-page

--- front/src/pages/Auction.js
+++ front/src/pages/Auction.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import './Auction.css';
+import { useStore } from '../utils/store';
+import CardTemplate from '../components/CardTemplate';
+import { dummydata } from '../utils/dummyData';
 
 function Auction() {
+
+    const id = useStore((state) => state.id)
+    console.log("auction-id : " + id);
+    const data = dummydata.nft[id-1];
+    console.log(data);
+
     return (
         <div className='page'>
             <h1 className='pageTitle'>
                 Auction
             </h1>
             <div className='auctionNFT'>
-                <div className='imgNFT'>image</div>
+                <div className='imgNFT'>
+                    <div className='listItem'>
+                        {data !== undefined ?
+                        <CardTemplate
+                            id={data.id}
+                            imgURI={data.imgURI}
+                            user={data.user}
+                            description={data.description}
+                            price={data.price}
+                            created_at={data.created_at}
+                        /> : <div></div>}
+                    </div>
+                </div>
                 <div className='profileNFT'>
                     <div className='dateNFT'>created date</div>
                     <div className='final-bid-container'>Winning bid</div>
