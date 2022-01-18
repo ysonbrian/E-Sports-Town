4a46b15981ac23c6435e769cb56bf1678c93c424
ysonbrian <znlsakxls@gmail.com>
2022-01-19 01:08:32 +0900

    최고가 수정

--- back/routes/auctionRouter.js
+++ back/routes/auctionRouter.js
@@ -31,21 +31,26 @@ router.post('/:id/bidding', async (req, res) => {
     // } else {
     //   res.send(false);
     // }
-    const [user] = await Users.find({ userAddress: currentAddress });
-    const [auction] = await auctionData.find({ tokenId: tokenId });
-
-    if (user?.token >= Number(bid)) {
-      if (auction?.length === 0) {
+    const user = await Users.find({ userAddress: currentAddress });
+    const auction = await auctionData.find({ tokenId: tokenId });
+    console.log(auction);
+    if (auction.length === 0) {
+      setBidding(req, res, req.body.metadata);
+    } else if (auction[0]?.biddingList?.length === 1) {
+      let max = auction[0]?.biddingList[0].bidPrice;
+      if (max >= Number(bid)) {
+        return res.send({ message: 'lowerThanMax' });
+      } else {
+        setBidding(req, res, req.body.metadata);
+      }
+    } else if (user[0]?.token >= Number(bid)) {
+      let max = auction[0]?.biddingList?.reduce((acc, val) => {
+        return acc > val.bidPrice ? acc : val.bidPrice;
+      });
+      if (max < Number(bid)) {
         setBidding(req, res, req.body.metadata);
       } else {
-        let max = auction?.biddingList.reduce((acc, val) => {
-          return acc > val.bidPrice ? acc : val.bidPrice;
-        });
-        if (max < Number(bid)) {
-          setBidding(req, res, req.body.metadata);
-        } else {
-          return res.send({ message: 'lowerThanMax' });
-        }
+        return res.send({ message: 'lowerThanMax' });
       }
     } else {
       return res.send({ message: 'NoMoney' });
